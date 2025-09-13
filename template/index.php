17791f58c535a2caf7290fb46d3c370fe5b23663
Usman Ahmed <syedusmanahmed@hotmail.com>
2025-09-13 08:53:25 +0500

    Proceedings

--- template/index.php
+++ template/index.php
@@ -7,11 +7,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>DevDocs &#8211; Easy to find documentation for Usman</title>
     <meta name='robots' content='noindex, nofollow' />
-    <link rel="stylesheet" id="style-css" href="<?php echo SITE_URL; ?>/template/assets/style.css" type="text/css" media="all" />
-    <script>
-        var site_url = "<?php echo SITE_URL; ?>";
-        var site_name = "DevDocs";
-    </script>
+    <link rel="stylesheet" href="<?php echo SITE_URL; ?>/template/assets/style.css" type="text/css" media="all" />
+    <link rel="stylesheet" href="<?php echo SITE_URL; ?>/template/assets/prism.css" type="text/css" media="all" />
+    <script type='text/javascript' src='<?php echo SITE_URL; ?>/template/assets/prism.js'></script>
 
     <link rel="icon" type="image/png" href="<?php echo SITE_URL; ?>/favicon.png">
 
@@ -22,32 +20,39 @@
     <div id="container">
         <div id="skill">
             <?php echo load_skill($_SERVER['REQUEST_URI']); ?>
-            <?php // include 'skill.php'; ?>            
         </div>
 
     </div>
-    <script type='text/javascript' src='<?php echo SITE_URL; ?>/template/assets/jquery.js'></script>
-    <!-- <script type='text/javascript' src='<?php echo SITE_URL; ?>/template/assets/main.js'></script> -->
+
     <script>
-        let ajax_link;
-        let content_container = $('#content');
-        $('#side-list a').on('click', function (e) {
-            e.preventDefault();
+        let ajax_links = document.querySelectorAll('#side-list a');
+        const contentDiv = document.getElementById('content');
 
-            ajax_link = $(this).attr('href');
+        for (let ajax_link of ajax_links) {
+            ajax_link.addEventListener('click', function(e) {
+                e.preventDefault();
+                let url = this.getAttribute('href');
+                window.history.pushState("object or string", "Title", url);
 
-            $.ajax({
-                url: ajax_link,
-                type: "POST",
-                data: {request_type: 'ajax'},
-                success: function(response) {
-                    content_container.html(response);
-                },
-                error: function(jqXHR, textStatus, errorThrown) {
-                    console.error("Error:", textStatus, errorThrown);
-                }
+                fetch(url, {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/x-www-form-urlencoded'
+                    },
+                    body: 'request_type=ajax'
+                })
+                .then(response => response.text())
+                .then(html => {
+                    contentDiv.innerHTML = html;
+
+                    Prism.highlightAll(contentDiv);
+                })
+                .catch(error => {
+                    console.error('Error fetching content:', error);
+                });
             });
-        });
+        }
+        
     </script>
 </body>
 
